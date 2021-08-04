8f8224f9d9bf47e6016f9a26bc585c53e4804a4c
Jakub Jóźwiak <jakub.lazy@gmail.com>
2021-08-04 21:35:08 +0200

    Curently weather are made
    I Add properties for wind, pressure and humidity also style rest of properties like temperature and icon

--- src/js/weather.js
+++ src/js/weather.js
@@ -6,23 +6,28 @@ export class Weather {
         this.temperature = document.querySelector('.weather-temperature__number')
         this.sunCycleTime = document.querySelectorAll('.weather-today__cycle-time')
         this.date = document.querySelector('.weather-today__date-name')
-
+        this.tempIcon = document.querySelector('.weather-temperature__icon')
+        this.humidity = document.querySelector('.weather-temperature__property-humidity--bold')
+        this.wind = document.querySelector('.weather-temperature__property-wind--bold')
+        this.pressure = document.querySelector('.weather-temperature__property-pressure--bold')
         this.setDate()
     }
 
     findLocalization(localization) {
-        return fetch(`${this.url}weather?q=${localization}&appid=${this.apiKey}`)
+        fetch(`${this.url}weather?q=${localization}&appid=${this.apiKey}`)
             .then(resp => resp.json())
             .then((data) => {
                 this.localization.textContent = `${data.name},`
                 this.setTemperature(data.main.temp)
                 this.setCycleSun([data.sys.sunrise, data.sys.sunset])
+                this.setIcon(`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+                this.setProperties(data.main.humidity, data.wind.speed, data.main.pressure)
             })
     }
 
     setTemperature(temp) {
         let realTemperature = temp / 10
-        this.temperature.textContent = realTemperature.toFixed(0)
+        this.temperature.innerHTML += `${realTemperature.toFixed(0)}`
     }
 
     setCycleSun(cycleSun) {
@@ -39,6 +44,16 @@ export class Weather {
     setDate() {
         let now = new Date();
         this.date.textContent = `Today ${now.getDate()}.${now.getMonth()}.${now.getFullYear()}`
+    }
+
+    setIcon(icon) {
+        this.tempIcon.setAttribute('src', icon)
+    }
+
+    setProperties(humidity, wind, pressure) {
+        this.humidity.textContent = `${humidity}%`;
+        this.wind.textContent = `${wind}km/h`
+        this.pressure.textContent = `${pressure}km/h`
     }
 
 }
