82bc06d23845d9cfb99f60d2343b7f0d7a53a5c7
Jakub Jóźwiak <jakub.lazy@gmail.com>
2021-08-16 23:13:58 +0200

    Add style for navigation News

--- src/js/category-news.js
+++ src/js/category-news.js
@@ -4,14 +4,17 @@ import {
 
 export class CategoryNews {
     constructor() {
-        this.newsApiCategoryItems = document.querySelectorAll('.nav-news__items');
+        this.newsApiCategoryItems = document.querySelectorAll('.nav-news__item');
         this.news = new News;
         this.chooseCategory()
     }
     chooseCategory() {
         for (let i = 0; i < this.newsApiCategoryItems.length; i++) {
+
             this.newsApiCategoryItems[i].addEventListener('click', () => {
                 const category = this.newsApiCategoryItems[i].dataset.option;
+                this.newsApiCategoryItems.forEach(item => item.classList.remove('nav-news__item--active'))
+                this.newsApiCategoryItems[i].classList.add('nav-news__item--active');
                 this.news.getDataFromApiByCategory(category)
             });
 
