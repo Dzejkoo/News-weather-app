27cb6cf64b9fc6b97bd698769f4fbea7ba4d88c1
Jakub Jóźwiak <jakub.lazy@gmail.com>
2021-08-20 10:45:40 +0200

    Add bgc for nav news, create function for seting images artile

--- src/js/news.js
+++ src/js/news.js
@@ -1,12 +1,28 @@
 export class News {
     constructor() {
         this.apiKey = 'cafd73a1a4bb442999ce4c3167c3344e';
+        this.articleImg = document.querySelectorAll('.news-article__image');
+        this.fromWhereArticle = document.querySelectorAll('.news-article__from-where');
 
     }
     getDataFromApiByCategory(category) {
         fetch(`https://newsapi.org/v2/top-headlines?country=pl&category=${category}&apiKey=${this.apiKey}`)
             .then(resp => resp.json())
-            .then(data => console.log(data))
+            .then((data) => {
+                // this.setPropertyForNews(data)
+                this.setImageForArticle(data.articles)
+            })
     }
 
+    // setPropertyForNews(news) {
+    //     const textTittle = news.articles[0].title
+    //     console.log(textTittle, news)
+    // }
+
+    setImageForArticle(articles) {
+        for (let i = 0; i < 3; i++) {
+            this.articleImg[i].setAttribute('src', articles[i].urlToImage)
+        }
+
+    }
 }
