8d397e5b8e26f43d6893e9e9e296f9004b7bb6d0
Jakub Jóźwiak <jakub.lazy@gmail.com>
2021-08-22 20:29:09 +0200

    set content, title and source news for article, from API and cut this by substring to max characters

--- src/js/category-news.js
+++ src/js/category-news.js
@@ -6,11 +6,12 @@ export class CategoryNews {
     constructor() {
         this.newsApiCategoryItems = document.querySelectorAll('.nav-news__item');
         this.news = new News;
-        this.chooseCategory()
+        this.chooseCategory();
+        this.news.getDataFromApiByCategory('general');
+
     }
     chooseCategory() {
         for (let i = 0; i < this.newsApiCategoryItems.length; i++) {
-
             this.newsApiCategoryItems[i].addEventListener('click', () => {
                 const category = this.newsApiCategoryItems[i].dataset.option;
                 this.newsApiCategoryItems.forEach(item => item.classList.remove('nav-news__item--active'))
--- src/js/news.js
+++ src/js/news.js
@@ -3,26 +3,41 @@ export class News {
         this.apiKey = 'cafd73a1a4bb442999ce4c3167c3344e';
         this.articleImg = document.querySelectorAll('.news-article__image');
         this.fromWhereArticle = document.querySelectorAll('.news-article__from-where');
+        this.titleArticle = document.querySelectorAll('.news-article__text-title');
+        this.expansionArticle = document.querySelectorAll('.news-article__text-expansion')
 
     }
     getDataFromApiByCategory(category) {
         fetch(`https://newsapi.org/v2/top-headlines?country=pl&category=${category}&apiKey=${this.apiKey}`)
             .then(resp => resp.json())
             .then((data) => {
-                // this.setPropertyForNews(data)
-                this.setImageForArticle(data.articles)
+                this.setPropertyForNews(data.articles)
             })
     }
 
-    // setPropertyForNews(news) {
-    //     const textTittle = news.articles[0].title
-    //     console.log(textTittle, news)
-    // }
+    setPropertyForNews(articles) {
 
-    setImageForArticle(articles) {
-        for (let i = 0; i < 3; i++) {
-            this.articleImg[i].setAttribute('src', articles[i].urlToImage)
+        for (let i = 0; i < this.articleImg.length; i++) {
+
+            //set content, title and  for article, from API and cut this by substring to max characters 
+            this.titleArticle[i].textContent = `${articles[i].title.substring(0, 40)}...`
+            this.expansionArticle[i].textContent = `${articles[i].content.substring(0, 80)}...`
+            this.fromWhereArticle[i].textContent = `${articles[i].source.name}`;
+
+            this.articleImg[i].setAttribute('src', '')
+
+            if (typeof articles[i].urlToImage === 'string') {
+                this.articleImg[i].setAttribute('src', articles[i].urlToImage)
+
+            } else {
+                this.articleImg[i].setAttribute('src', './src/images/no-image.jpg')
+            }
         }
 
     }
+    // setTextForArticles(title) {
+    //     let maxTitleLetter = title.substring(0, 40);
+    //     this.titleAricle.textContent = `${maxTitleLetter}...`
+
+    // }
 }
